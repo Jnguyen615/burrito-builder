01310da9517aff4e530c6ed5499fa3d6489db661
Jen Nguyen <jenniferannnguyen@live.com>
2023-11-13 14:33:36 -0700

    Start cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
 describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-  });
+ beforeEach(() => {
+  cy.intercept('GET', 'http://localhost:3001/avi/v1/orders', {
+    statusCode: 200,
+    fixutre: 'orders'
+  })
+  cy.visit('http://localhost:3000')
+ })
+ it('should display the home page with some orders' ,() =>{
+  cy.get('h1')
+  .should('contain', 'Burrito Builder')
+ })
 });
